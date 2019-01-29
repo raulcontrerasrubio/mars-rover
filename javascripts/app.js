c7f6e02b3441b2c3809c018968a3adf729ad1958
Raúl Contreras <raulc98@gmail.com>
2019-01-29 19:20:54 +0100

    Finish iteration 4. The rover can move forward, turn left and right and prepare it's next movements. Also there is a map generator and the rover's default position is random within the bounds of the map

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,104 @@
+var map = createMap(10, 10);
+
 // Rover Object Goes Here
 // ======================
-
+var rover = {
+  x: getRandomPosition(map).col,
+  y: getRandomPosition(map).row,
+  direction: "N"
+};
 // ======================
 function turnLeft(rover){
-  console.log("turnLeft was called!");
+  switch(rover.direction){
+    case 'N':
+      rover.direction = 'W';
+    break;
+    case 'W':
+      rover.direction = 'S';
+    break;
+    case 'S':
+      rover.direction = 'E';
+    break;
+    case 'E':
+      rover.direction = 'N';
+    break;
+    default:
+      console.log("Rover direction is broken!!");
+    break;
+  }
 }
 
 function turnRight(rover){
-  console.log("turnRight was called!");
+  switch(rover.direction){
+    case 'N':
+      rover.direction = 'E';
+    break;
+    case 'W':
+      rover.direction = 'N';
+    break;
+    case 'S':
+      rover.direction = 'W';
+    break;
+    case 'E':
+      rover.direction = 'S';
+    break;
+    default:
+      console.log("Rover direction is broken!!");
+    break;
+  }
 }
 
 function moveForward(rover){
-  console.log("moveForward was called")
+  switch(rover.direction){
+    case 'N':
+      rover.y -= 1; // El sistema de coordenadas es, hacia arriba, menos y. Hacia la derecha, más x
+    break;
+    case 'W':
+      rover.x -= 1;
+    break;
+    case 'S':
+      rover.y += 1;
+    break;
+    case 'E':
+      rover.x += 1;
+    break;
+    default:
+      console.log("Rover engine is broken!!");
+    break;
+  }
+}
+
+function createMap(rows, cols){
+  var map = [];
+  for(var i = 0; i < rows; i += 1){
+    map.push([]);
+    for(var j = 0; j < cols; j += 1){
+      map[i].push(0);
+    }
+  }
+
+  return map;
+}
+
+function getRandomPosition(grid){
+  var row = Math.floor(Math.random() * grid.length);
+  var col = Math.floor(Math.random() * grid[row].length);
+  return {row,col};
+}
+
+function prepareMoves(rover, list){
+  list.split('').forEach(move => {
+    switch(move){
+      case 'f':
+        moveForward(rover);
+      break;
+      case 'l':
+        turnLeft(rover);
+      break;
+      case 'r':
+        turnRight(rover);
+      break;
+    }
+    // console.log(rover);
+  });
 }
